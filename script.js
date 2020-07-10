82cc17885ffa70c83d962d00b05c6c527016a6e6
Glitch (hello-webpage) <none>
2020-07-10 23:21:07 +0000

    🛩🙋 Checkpoint ./script.js:18092373/325

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* global createCanvas, strokeWeight, stroke, sq, windowWidth, line, windowHeight, colorMode, HSB,pmouseX, pmouseY background loadImage image resizeCanvas get mouseX, mouseY*/
 let imgDimensions = { w: 640, h: 329 };
-let userUrl =
+let imgUrl =
   "https://cdn.glitch.com/f979082c-4790-4538-b5e9-dcd548a7bef6%2Fgoogle-background-1467806874.jpg?v=1594410010295";
 let canvas,
   displayImg,
@@ -9,8 +9,9 @@ let canvas,
   brushWeight = 4,
   lastColor = [128, 128, 128, 255];
 
+
 function preload() {
-  displayImg = loadImage(userUrl);
+  displayImg = loadImage(imgUrl);
 }
 
 function setup() {
@@ -37,34 +38,34 @@ function getDimensions(url) {
 }
 
 async function userUpload() {
-  userUrl = window.prompt("Enter an image URL:");
-  await getDimensions(userUrl);
-  displayImg = loadImage(userUrl);
+  imgUrl = window.prompt("Enter an image URL:");
+  await getDimensions(imgUrl);
+  displayImg = loadImage(imgUrl);
   adjustCanvas();
 }
 
 async function randomUpload() {
   let randWidth = Math.floor(Math.random() * windowWidth + 150)
   let randHeight = Math.floor(Math.random() * windowHeight + 200)
-  userUrl = "https://picsum.photos/" + randWidth + "/" + randHeight;
-  await getDimensions(userUrl);
-  displayImg = loadImage(userUrl);
+  imgUrl = "https://picsum.photos/" + randWidth + "/" + randHeight;
+  await getDimensions(imgUrl);
+  displayImg = loadImage(imgUrl);
   adjustCanvas();
 }
 
 function revealColor() {
   pixel = displayImg.get(mouseX, mouseY); //}
-  pixel = averageStroke(pixel);
+  pixel = averageColor(pixel, lastColor);
   stroke(pixel);
   if(onCanvas){
   line(pmouseX, pmouseY, mouseX, mouseY);}
   lastColor = pixel;
 }
 
-function averageColor(colorIn) {
+function averageColor(color1, color2) {
   let avgColor = [];
   for (let i = 0; i < lastColor.length; i++) {
-    avgColor[i] = Math.sqrt((sq(colorIn[i]) + sq(lastColor[i])) / 2);
+    avgColor[i] = Math.sqrt((sq(color1[i]) + sq(color2[i])) / 2);
   }
   return avgColor;
 }
@@ -80,8 +81,9 @@ function adjustCanvas() {
   document.getElementById("canvas-div").style =
     "width: " + imgDimensions.w + "px";
   document.getElementById("canvas-div").style =
-    "height: " + imgDimensions.h + "px";
+    "height: " + imgDimensions.h-100 + "px";
 }
 
+//prevents black lines when mouse moves off canvas
 document.getElementById("canvas-div").addEventListener("mouseenter", function() {onCanvas=true;});
 document.getElementById("canvas-div").addEventListener("mouseout", function() {onCanvas=false;});
