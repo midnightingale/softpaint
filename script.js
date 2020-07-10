1b14d1dcda7f015dab075223fe837f1f343f74d0
Glitch (hello-webpage) <none>
2020-07-10 19:15:48 +0000

    🗺💘 Checkpoint ./script.js:18092373/707 ./index.html:18092373/1720

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+/*global image, loadImage*/
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+let img;
+function preload() {
+  img = loadImage('https://talkingbiznews.com/wp-content/uploads/2019/10/Google.jpg');
+}
+function setup() {
+  // Top-left corner of the img is at (0, 0)
+  // Width and height are the img's original width and height
+  createCanvas(500,500)
+  image(img, 0, 0);
+}
+
+function draw(){
+  background(0)
+  image(img, 0, 0);
+}
