614bf6007e881776c74801ac46ab0c73a3aac899
Glitch (softpoints-feature-work) <none>
2020-07-12 21:55:40 +0000

    🍨🏄 Checkpoint ./script.js:18092373/16333 ./defaults.js:18092373/7460 ./index.html:18092373/126

---
+++ defaults.js
@@ -0,0 +1,21 @@
+let defaultPaintings = [
+  "https://cdn.glitch.com/ed00bc65-49f7-4b45-ad53-f1dcea7aba31%2Fil_570xN.1424060518_k0dd.jpg?v=1594424247188",
+  "https://upload.wikimedia.org/wikipedia/commons/7/77/Vincent_van_Gogh_-_Van_Gogh%27s_Bedroom_in_Arles_-_Google_Art_Project.jpg",
+  "https://collectionapi.metmuseum.org/api/collection/v1/iiif/488730/1004971/main-image",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Nighthawks_by_Edward_Hopper_1942.jpg/1920px-Nighthawks_by_Edward_Hopper_1942.jpg",
+  "https://upload.wikimedia.org/wikipedia/en/d/dc/Woman_in_Hat_and_Fur_Collar.jpg",
+  "https://cdn.glitch.com/78391c90-30ed-44d7-8ca3-ccd51ddd2e05%2F4c90350d7c32a9b07b48470ca17469eb.jpg",
+  "https://cdn.glitch.com/78391c90-30ed-44d7-8ca3-ccd51ddd2e05%2Fclocks-2.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Edwin_Blashfield_-_Spring_Scattering_Stars.jpg/755px-Edwin_Blashfield_-_Spring_Scattering_Stars.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/7/75/Claude_Monet%2C_Le_Grand_Canal.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/28/Sunrise_on_the_Matterhorn_MET_DT218107.jpg/800px-Sunrise_on_the_Matterhorn_MET_DT218107.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Albert_Bierstadt_-_Mount_Corcoran.jpg/1200px-Albert_Bierstadt_-_Mount_Corcoran.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Gosnold_at_Cuttyhunk.jpg/1200px-Gosnold_at_Cuttyhunk.jpg",
+  "https://en.wikipedia.org/wiki/File:Albert_Bierstadt_-_California_Spring_-_Google_Art_Project.jpg",
+  "https://en.wikipedia.org/wiki/File:Albert_Bierstadt_-_Valley_of_the_Yosemite_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Claude-Joseph_Vernet_-_The_four_times_of_day-_Evening_-_Google_Art_Project.jpg/1200px-Claude-Joseph_Vernet_-_The_four_times_of_day-_Evening_-_Google_Art_Project.jpg",
+  "https://uploads7.wikiart.org/images/emily-carr/chemainus-bay-vancouver-island-1925.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Boris_Kustodiev_-_Shrovetide_-_Google_Art_Project.jpg/1200px-Boris_Kustodiev_-_Shrovetide_-_Google_Art_Project.jpg",
+  "https://en.wikipedia.org/wiki/File:Claude-Joseph_Vernet_-_The_four_times_of_day-_Evening_-_Google_Art_Project.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Paul_Gauguin_-_Fatata_te_Miti_%28By_the_Sea%29_-_Google_Art_Project.jpg/1280px-Paul_Gauguin_-_Fatata_te_Miti_%28By_the_Sea%29_-_Google_Art_Project.jpg"
+];
